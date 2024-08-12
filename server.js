8ed5646065ff8708d1549dd16fa2a3525df79568
Amalkrishnanvp <Amalyedhu10@gmail.com>
2024-08-12 08:21:19 +0530

    Dynamically served images and image description using handlebars

--- server.js
+++ server.js
@@ -3,12 +3,27 @@ import express from "express";
 const app = express();
 const port = 1000;
 
+const container = {
+  title: "Image Gallery",
+  heading: "My Image Gallery",
+  imageHolder: [
+    { image: "img1", description: "Car" },
+    { image: "img2", description: "Hils" },
+    { image: "img3", description: "City" },
+    { image: "img4", description: "Bulb" },
+    { image: "img5", description: "House" },
+    { image: "img6", description: "Plant" },
+    { image: "img7", description: "Fish" },
+    { image: "img8", description: "Bike" },
+  ],
+};
+
 app.set("view engine", "hbs");
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("main", { title: "Image Gallery", heading: "My Image Gallery" });
+  res.render("main", { accesser: container });
 });
 
 app.listen(port, () => {
